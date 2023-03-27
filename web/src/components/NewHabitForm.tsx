6ca149e2acf689764c607596449598427b9a94f4
Renisson Silva <renissonx@gmail.com>
2023-03-26 22:55:24 -0300

    Fazendo chamada de store do hábito, trazendo algumas informações dos popovers e traduzindo dias para pt-br

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import * as Checkbox from "@radix-ui/react-checkbox"
 import { Check } from "phosphor-react"
 import { FormEvent, useState } from "react"
+import { api } from "../lib/axios";
 
 const availableWeekDays = [
     'Domingo',
@@ -17,10 +18,22 @@ export function NewHabitForm () {
     const [title, setTitle] = useState('');
     const [weekDays, setWeekDays] = useState<number[]>([]);
 
-    function createNewHabit(event: FormEvent) {
+    async function createNewHabit(event: FormEvent) {
         event.preventDefault();
     
-        console.log(weekDays);
+        if(!title || weekDays.length === 0) {
+            return
+        }
+
+        await api.post('habits', {
+            title,
+            weekDays
+        })
+
+        setTitle('')
+        setWeekDays([])
+
+        alert('Hábito criado')
     }
 
     function handleToggleWeekDay(weekDay: number) {
@@ -48,6 +61,7 @@ export function NewHabitForm () {
                 className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
                 autoFocus
                 onChange={event => setTitle(event.target.value)}
+                value={title}
             />
 
             <label htmlFor="" className="font-semibold leading-tight mt-4">
@@ -60,6 +74,7 @@ export function NewHabitForm () {
                         <Checkbox.Root 
                             key={weekDay} 
                             className="flex items-center gap-3 group"
+                            checked={weekDays.includes(index)}
                             onCheckedChange={() => handleToggleWeekDay(index)}
                         >
                             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
