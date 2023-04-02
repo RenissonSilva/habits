c9b46e64161bf213f1f7574a24cb7f2aa435d34a
Renisson Silva <renissonx@gmail.com>
2023-04-01 22:33:42 -0300

    Atualizando componentes ao marcar/desmarcar hábitos, adicionada transições para deixar mais suave quando alterar a cor dos quadrados e a barra de progressão, estilização dos elementos em foco

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from "react";
 import { api } from "../lib/axios";
 
 interface HabitsListProps {
-    date:Date
+    date: Date,
+    onCompĺetedChanged: (completed: number) => void
 }
 
 interface HabitsInfo {
@@ -17,7 +18,7 @@ interface HabitsInfo {
     completedHabits: string
 }
 
-export function HabitsList({ date }: HabitsListProps) {
+export function HabitsList({ date, onCompĺetedChanged }: HabitsListProps) {
     const [ habitsInfo, setHabitsInfo ] = useState<HabitsInfo>()
 
     useEffect(() => {
@@ -46,6 +47,8 @@ export function HabitsList({ date }: HabitsListProps) {
             possibleHabits: habitsInfo!.possibleHabits,
             completedHabits
         })
+
+        onCompĺetedChanged(completedHabits.length)
     }
 
     const isDateInPast = dayjs(date)
@@ -60,10 +63,10 @@ export function HabitsList({ date }: HabitsListProps) {
                         key={habit.id}
                         onCheckedChange={() => handleToggleHabit(habit.id)}
                         checked={habitsInfo.completedHabits.includes(habit.id)}
-                        className="flex items-center gap-3 group" 
+                        className="flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed" 
                         disabled={isDateInPast}
                     >
-                        <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                        <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 group-focus:ring-2 group-focus:ring-violet-500 group-focus:ring-offset-2 group-focus:ring-offset-background">
                             <Checkbox.Indicator>
                                 <Check size={20} className="text-white" />
                             </Checkbox.Indicator>
